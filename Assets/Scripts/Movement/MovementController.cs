f58ff125c83a804acaca06f2d5315d8e939f1c5d
Mad-way <mkaygorodskiy@mail.ru>
2022-05-18 20:07:03 +0300

    removal of excess

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -42,6 +42,7 @@ public class MovementController : MonoBehaviour
     {
         yield return new WaitForSeconds(45);
         enjineStart = false;
+        startButton.interactable = true;
     }
 
     public void SwitchEngineStart() {
